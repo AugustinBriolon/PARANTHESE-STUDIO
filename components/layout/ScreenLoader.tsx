414866022e1acd60fdce68eca60b0edd0f9cf99c
Augustin Briolon <augustin.briolon@gmail.com>
2025-10-24 23:05:44 +0200

    improve animation and add capitalize for screen loader

--- components/layout/ScreenLoader.tsx
+++ components/layout/ScreenLoader.tsx
@@ -13,7 +13,7 @@ const initialPath =
   'M117 377.8C83.4 361.533 55.5333 337 33.4 304.2C11.5333 271.4 0.600001 233 0.600001 189C0.600001 145 11.5333 106.6 33.4 73.8C55.5333 41 83.4 16.4667 117 0.199994L135.8 33C107.533 47.9333 84.7333 68.8667 67.4 95.8C50.0667 122.467 41.4 153.533 41.4 189C41.4 224.467 50.0667 255.667 67.4 282.6C84.7333 309.267 107.533 330.067 135.8 345L117 377.8Z';
 const transformedPath = 'M0 378V0H40V378H0Z';
 
-export const timeToLoad = 3.5;
+export const timeToLoad = 3.9;
 
 const ScreenLoader = () => {
   const isScreenLoader = useIsScreenLoader();
@@ -68,18 +68,18 @@ const ScreenLoader = () => {
           split.chars,
           {
             yPercent: 100,
-            stagger: 0.01,
-            duration: 0.5,
-            ease: 'power2.inOut',
+            stagger: 0.02,
+            duration: 0.8,
+            ease: 'power2.out',
           },
-          '<',
+          '>-0.2',
         )
         .addLabel('transformParanthese')
         .to(
           textContainerRef.current,
           {
             width: 0,
-            duration: 0.5,
+            duration: 0.8,
             ease: 'power2.inOut',
           },
           'transformParanthese',
@@ -98,6 +98,7 @@ const ScreenLoader = () => {
           {
             x: (i) => ['-15%', '15%'][i],
             duration: 0.5,
+            ease: 'power2.inOut',
           },
           'transformParanthese',
         )
@@ -108,29 +109,37 @@ const ScreenLoader = () => {
             scaleY: 50,
             duration: 0.8,
             ease: 'power2.inOut',
+            onComplete: () => {
+              gsap.set([parantheseRefs.left.svg.current, parantheseRefs.right.svg.current], {
+                display: 'none',
+              });
+            },
           },
-          '>-0.03',
+          '>+0.2',
         )
-        .set([parantheseRefs.left.svg.current, parantheseRefs.right.svg.current], {
-          display: 'none',
-        })
         .to(
           [transitionDivRefs.left.current, transitionDivRefs.right.current],
           {
             scaleX: 100,
             duration: 1,
             ease: 'power2.inOut',
+            onComplete: () => {
+              gsap.set(backgroundRef.current, {
+                display: 'none',
+              });
+            },
           },
-          '>',
+          '>-0.4',
         )
-        .set(backgroundRef.current, {
-          display: 'none',
-        })
-        .to([transitionDivRefs.left.current, transitionDivRefs.right.current], {
-          x: (i) => [-1100, 1100][i],
-          duration: 0.8,
-          ease: 'power2.inOut',
-        })
+        .to(
+          [transitionDivRefs.left.current, transitionDivRefs.right.current],
+          {
+            x: (i) => [-1100, 1100][i],
+            duration: 1.2,
+            ease: 'power2.inOut',
+          },
+          '>-0.1',
+        )
         .set(screenLoaderRef.current, {
           display: 'none',
           duration: 0,
@@ -167,7 +176,7 @@ const ScreenLoader = () => {
         <div ref={textContainerRef} className="flex justify-center overflow-hidden">
           <h1 ref={mainTextRef} className="text-center whitespace-nowrap opacity-0">
             <span className="w-4 md:w-10"></span>
-            Paranthese Studio
+            PARANTHESE STUDIO
             <span className="w-4 md:w-10"></span>
           </h1>
         </div>
