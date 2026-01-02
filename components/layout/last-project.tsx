4bbaa6fff1fcc0109ae6d6235af040889e5e0db7
Augustin Briolon <augustin.briolon@gmail.com>
2026-01-02 18:56:21 +0100

    add transiton page

--- components/layout/last-project.tsx
+++ components/layout/last-project.tsx
@@ -4,7 +4,7 @@ import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import SplitText from 'gsap/SplitText';
 import { useEffect, useRef, useState } from 'react';
-import Button from '../ui/Button';
+import Button from '../ui/button';
 import { TIME_TO_LOAD } from '@/constants/time-to-load';
 
 export default function LastProject() {
