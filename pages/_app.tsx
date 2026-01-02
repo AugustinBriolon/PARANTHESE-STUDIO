c9f92c415ff1f7ca82db3d04504c3f0ea3d41064
Augustin Briolon <augustin.briolon@gmail.com>
2026-01-02 19:03:02 +0100

    add transiton page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,6 @@ import { useRouter } from 'next/router';
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const [pageKey] = router.asPath.split('?');
-  console.log(router, pageKey);
 
   return (
     <>
